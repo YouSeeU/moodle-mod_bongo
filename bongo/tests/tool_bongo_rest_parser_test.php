37cec9186c7ec01587055e474343f415eadb0e62
Brian Kelly <wolfyman06@gmail.com>
2018-10-16 12:52:16 -0600

    add more asserts to rest parser

--- bongo/tests/tool_bongo_rest_parser_test.php
+++ bongo/tests/tool_bongo_rest_parser_test.php
@@ -8,18 +8,31 @@ require_once($CFG->dirroot.'/admin/tool/bongo/locallib.php');
 
 class tool_bongo_rest_parser_testcase extends basic_testcase {
     function tool_bongo_parse_rest_response() {
-        $this->resetAfterTest(true);
+        $url = 'https://bongo-test-site.youseeu.com/lti/school-name/connect';
+        $key = '1234567890';
+        $secret = 'ABCDEFGHI';
+        $region = 'na';
 
         $response = '"body": '.
             '"{ " '.
-                ' \"url\": \"https://bongo-test-site.youseeu.com/lti/school-name/connect\",'.
-                ' \"key\": \"1234567890\",'.
-                ' \"secret\": \"ABCDEFGHI\",'.
-                ' \"region\": \"na\"'.
+                ' \"url\": \"' . $url . '\",'.
+                ' \"key\": \"' . $key . '\",'.
+                ' \"secret\": \"' . $secret . '\",'.
+                ' \"region\": \"' . $region . '\"'.
             '}"';
 
         $parsedresponse = tool_bongo_parse_response($response);
 
-        assert($parsedresponse->errorexists == false, 'Rest error exists in parsed object!');
+        assert($parsedresponse->errorexists == false, 'Rest error exists in parsed object!' . var_dump($parsedresponse));
+        assert(!is_null($parsedresponse->url), 'Rest URL does not exist!' . var_dump($parsedresponse));
+        assert(!is_null($parsedresponse->key), 'Rest Key was not parsed correctly!' . var_dump($parsedresponse));
+        assert(!is_null($parsedresponse->secret), 'Rest secret was not parsed correctly!' . var_dump($parsedresponse));
+        assert(!is_null($parsedresponse->region), 'Rest region was not parsed correctly!' . var_dump($parsedresponse));
+
+        assert($parsedresponse->url == $url, 'Rest URL was not parsed correctly!' . var_dump($parsedresponse));
+        assert($parsedresponse->key == $key, 'Rest Key was not parsed correctly!' . var_dump($parsedresponse));
+        assert($parsedresponse->secret == $secret, 'Rest secret was not parsed correctly!' . var_dump($parsedresponse));
+        assert($parsedresponse->region == $region, 'Rest region was not parsed correctly!' . var_dump($parsedresponse));
+
     }
 }
