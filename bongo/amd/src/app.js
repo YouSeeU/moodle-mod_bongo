8b14e50386ae050b4a190b150450163626bbfaf7
Zach Hittle <zachary.hittle@youseeu.com>
2018-10-24 15:48:37 -0600

    disable button on form submit rather than button click

--- bongo/amd/src/app.js
+++ bongo/amd/src/app.js
@@ -2,9 +2,10 @@ define(['jquery'], function ($) {
 
   return {
     init: function () {
+      var form = $('#mform1');
       var submitButton = $('#id_submitbutton');
-      submitButton.click(function () {
-        $('#mform1').submit();
+
+      form.submit(function () {
         submitButton.prop('disabled', true);
       });
     }
