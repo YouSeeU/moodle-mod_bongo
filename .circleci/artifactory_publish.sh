5759ca81488ff5d29bd3ea8d5d510cbf936550b2
Brian Kelly <wolfyman06@gmail.com>
2018-10-26 15:36:35 -0600

    publish zips

--- .circleci/artifactory_publish.sh
+++ .circleci/artifactory_publish.sh
@@ -6,5 +6,5 @@ curl -fL https://getcli.jfrog.io | sh
 ./jfrog rt c --url ${ARTIFACTORY_URL} --user ${ARTIFACTORY_USERNAME} --password ${ARTIFACTORY_PASSWORD} --interactive false
 ./jfrog rt bce ${CIRCLE_PROJECT_REPONAME} ${CIRCLE_BUILD_NUM}
 ./jfrog rt bag ${CIRCLE_PROJECT_REPONAME} ${CIRCLE_BUILD_NUM}
-./jfrog rt u --build-name=${CIRCLE_PROJECT_REPONAME} --build-number=${CIRCLE_BUILD_NUM} --flat=true --recursive=false \*.tgz ${ARTIFACTORY_REPO}/${ARTIFACTORY_ORG}/${MOODLE_MODULE}/
+./jfrog rt u --build-name=${CIRCLE_PROJECT_REPONAME} --build-number=${CIRCLE_BUILD_NUM} --flat=true --recursive=false \*.zip ${ARTIFACTORY_REPO}/${ARTIFACTORY_ORG}/${MOODLE_MODULE}/
 ./jfrog rt bp  ${CIRCLE_PROJECT_REPONAME} ${CIRCLE_BUILD_NUM}
