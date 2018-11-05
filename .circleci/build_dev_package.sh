eb5263764c4e51693500cf5358269e85fcc99cea
Brian Kelly <wolfyman06@gmail.com>
2018-11-05 16:41:11 -0700

    remove unnecessary shell scripts

--- .circleci/build_dev_package.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-set -ex
-
-MOODLE_MODULE=`jq '.name' -r package.json`
-MODULE_VERSION=`jq '.version' -r package.json | cut -d + -f 1`
-./package_plugin.sh $MOODLE_MODULE $MODULE_VERSION
