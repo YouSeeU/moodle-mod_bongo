f8dbb2785266f73294ce993cc1d6ddb1ca507424
Brian Kelly <wolfyman06@gmail.com>
2018-11-05 15:37:12 -0700

    add better checking to file name creation (#11)

--- .circleci/build_package.sh
+++ .circleci/build_package.sh
@@ -3,4 +3,4 @@ set -ex
 
 MOODLE_MODULE=`jq '.name' -r package.json`
 MODULE_VERSION=`jq '.version' -r package.json | cut -d + -f 1`
-./package_plugin.sh $MOODLE_MODULE $MODULE_VERSION
+../package_plugin.sh $MOODLE_MODULE $MODULE_VERSION
--- package_plugin.sh
+++ package_plugin.sh
@@ -1,12 +1,27 @@
 #!/usr/bin/env bash
 
-MOODLE_MODULE=$1
+MOODLE_NAME=$1
 MODULE_VERSION=$2
 
+echo "Moodle Name: $MOODLE_NAME"
+echo "Module Version: $MODULE_VERSION"
+
+if [ -z "$MOODLE_NAME" ]
+then
+      echo "\$MOODLE_NAME is empty, defaulting to 'bongo'"
+      MOODLE_NAME="bongo"
+fi
+
+if [ -z "$MODULE_VERSION" ]
+then
+      echo "\$MODULE_VERSION is empty, defaulting to 'version'"
+      MODULE_VERSION="bongo"
+fi
+
 echo 'File name: '
-echo $MOODLE_MODULE-$MODULE_VERSION.zip
+echo $MOODLE_NAME-$MODULE_VERSION.zip
 
 #Zip all folder contents including top level plugin name directory
 rm -f bongo.zip
-rm -f $MOODLE_MODULE-$MODULE_VERSION.zip
-zip -r $MOODLE_MODULE-$MODULE_VERSION.zip bongo/
+rm -f $MOODLE_NAME-$MODULE_VERSION.zip
+zip -r $MOODLE_NAME-$MODULE_VERSION.zip bongo/
