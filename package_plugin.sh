79a805f875813862cac72505ec40da1c5a16ec12
Brian Kelly <wolfyman06@gmail.com>
2018-11-05 15:11:10 -0700

    Feature/build context and version (#7)
    * add build config changes for branches
    
    * add build context for circleci and artifact version

--- package_plugin.sh
+++ package_plugin.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
+MOODLE_MODULE=$1
+MODULE_VERSION=$2
+
 #Zip all folder contents including top level plugin name directory
 rm bongo.zip
-zip -r bongo.zip bongo/
+rm $MOODLE_MODULE-$MODULE_VERSION.zip
+zip -r $MOODLE_MODULE-$MODULE_VERSION.zip bongo/
