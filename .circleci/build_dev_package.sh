e2d5e6a59dd3a939b1b8dd2f79048ef828b52ea2
Brian Kelly <wolfyman06@gmail.com>
2018-11-05 15:22:44 -0700

    fix the shell script that is called because it was out of order

--- .circleci/build_dev_package.sh
+++ .circleci/build_dev_package.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -ex
 
-./package_plugin.sh
 MOODLE_MODULE=`jq '.name' -r package.json`
 MODULE_VERSION=`jq '.version' -r package.json | cut -d + -f 1`
+./package_plugin.sh $MOODLE_MODULE $MODULE_VERSION
