1b66f831453a1381e474fafa0f4131c766f1faf6
Brian Kelly <wolfyman06@gmail.com>
2018-11-05 15:18:57 -0700

    add print of file deletion (#9)

--- package_plugin.sh
+++ package_plugin.sh
@@ -3,6 +3,9 @@
 MOODLE_MODULE=$1
 MODULE_VERSION=$2
 
+echo 'File name: '
+echo $MOODLE_MODULE-$MODULE_VERSION.zip
+
 #Zip all folder contents including top level plugin name directory
 rm -f bongo.zip
 rm -f $MOODLE_MODULE-$MODULE_VERSION.zip
