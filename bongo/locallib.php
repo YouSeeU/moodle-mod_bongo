a9d8de6e8bafb3020caaa40ec60814e20a4e3b59
Brian Kelly <wolfyman06@gmail.com>
2018-12-10 09:18:08 -0700

    remove unnecessary check for login, file is already marked as internal

--- bongo/locallib.php
+++ bongo/locallib.php
@@ -41,7 +41,6 @@
 if (!defined('MOODLE_INTERNAL')) {
     die('Direct access to this script is forbidden.'); // It must be included from a Moodle page.
 }
-require_login();
 
 global $CFG, $DB;
 require_once($CFG->dirroot . '/mod/lti/locallib.php');
