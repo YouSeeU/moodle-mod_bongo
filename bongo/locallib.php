ad45408083f62a43bbe1505fd813087ef881b6d5
Brian Kelly <wolfyman06@gmail.com>
2018-12-10 09:20:55 -0700

    remove CFG and DB variables that are unnecessary

--- bongo/locallib.php
+++ bongo/locallib.php
@@ -42,7 +42,6 @@ if (!defined('MOODLE_INTERNAL')) {
     die('Direct access to this script is forbidden.'); // It must be included from a Moodle page.
 }
 
-global $CFG, $DB;
 require_once($CFG->dirroot . '/mod/lti/locallib.php');
 require_once($CFG->dirroot . '/course/lib.php');
 require_once($CFG->dirroot . '/course/modlib.php');
